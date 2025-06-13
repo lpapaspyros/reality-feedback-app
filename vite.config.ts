8cff59aeb2ef765b278e53944b185564a220b08d
Lampros Papaspyros <lamp.papaspyros@gmail.com>
2025-06-13 01:59:34 +0100

    Update dependencies and add PostCSS configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reality-feedback-app/', // replace with your repo name
+  base: '/reality-feedback-app/'  // This must match your GitHub repo name
 })
